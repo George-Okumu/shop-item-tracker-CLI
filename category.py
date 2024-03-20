1df46af6e043aa40ceffa607bc78a328cd26058f
George-Okumu <gokumu12@gmail.com>
2024-03-20 10:03:01 +0300

    feat: add uuid to generate unique ids, sec purposes

--- category.py
+++ category.py
@@ -1,7 +1,8 @@
+import uuid
+
 class Category():
     def __init__(self, name, description) -> None:
-        self.id = None
+        self.id = uuid.uuid4().hex
         self.name = name
         self.description = description
 
-        
--- item.py
+++ item.py
@@ -1,6 +1,8 @@
+import uuid
+
 class Item():
     def __init__(self, name, batchnumber, price, category) -> None:
-        self.id = None
+        self.id = uuid.uuid4().hex
         self.name = name
         self.batchnumber = batchnumber
         self.price = price
