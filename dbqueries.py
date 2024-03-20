04b429095fe74b8093148d9a8b1e36c973de3ca7
George-Okumu <gokumu12@gmail.com>
2024-03-20 11:03:44 +0300

    feat: add methods for inserting items and categories into db

--- dbqueries.py
+++ dbqueries.py
@@ -3,7 +3,7 @@ from config import CONNECTION as con, CURSOR as curs
 def create_table_items():
     curs.execute(
         """
-            CREATE TABLE IF NOT EXISTS items (id integer primary key, name varchar(32), batch_number TEXT, price integer, category_id integer, created_at timestamp)
+            CREATE TABLE IF NOT EXISTS items (id integer primary key, name varchar(32), batch_number TEXT, price integer, category_id integer, created_at timestamp default CURRENT_TIMESTAMP)
 
     """
     )
@@ -19,8 +19,26 @@ def create_table_categories():
     )
     
     con.commit()
+
+def insert_items(name, batch_number, price, category):
+    curs.execute(
+        """
+        INSERT INTO items(name, batch_number, price, category_id) VALUES (?, ?, ?, ?)
+        """,
+        (name, batch_number, price, category)
+    )
     
+    con.commit()
     
+def insert_categories(name, description):
+    curs.execute(
+        """
+        INSERT INTO categories (name, description) VALUES (?, ?)
+        """,
+        (name, description)
+    )
+    
+    con.commit()
     
 create_table_categories()
 create_table_items()
