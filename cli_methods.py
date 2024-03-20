da145f4555de13a7265d75d7a168f238aa6b2814
George-Okumu <gokumu12@gmail.com>
2024-03-20 12:34:14 +0300

    feat: add cli methods to be called in main class

--- cli_methods.py
+++ cli_methods.py
@@ -1,5 +1,6 @@
 from item import Item
 from category import Category
+from tabulate import tabulate
 
 def menu():
     print("Please select an option:")
@@ -7,14 +8,28 @@ def menu():
     print("1. See all stock")
     print("2. See all categories")
 
+def print_items_neat(data):
+    rows = [[item['id'], item['name'], item['batch_number'], item['price'], item['category']] for item in data]
+
+    headers = ["ID", "Name", "Batch Number", "Price", "Category"]
+
+    table = tabulate(rows, headers=headers, tablefmt="pretty")
+    print(table)
+
+def print_categories_neat(data):
+    rows = [[item['id'], item['name'], item['description']] for item in data]
+    headers = ["ID", "Name", "Description"]
+    table = tabulate(rows, headers=headers, tablefmt="pretty")
+    print(table)
+    
 
 def print_all_items():
     print("Here are all stock you have:")
-    print(Item.get_all_items())
+    print_items_neat(Item.get_all_items())
     
 def print_all_categories():
     print("Below are available category:")
-    print(Category.get_all_items())
+    print_categories_neat(Category.get_all_items())
     
     
 def exit():
