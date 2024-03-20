44eeb2aaf88d71ec00db67d9336209f9f78ee9fd
George-Okumu <gokumu12@gmail.com>
2024-03-20 09:55:44 +0300

    feat: model creation

---
+++ category.py
@@ -0,0 +1,7 @@
+class Category():
+    def __init__(self, name, description) -> None:
+        self.id = None
+        self.name = name
+        self.description = description
+
+        
---
+++ item.py
@@ -0,0 +1,7 @@
+class Item():
+    def __init__(self, name, batchnumber, price, category) -> None:
+        self.id = None
+        self.name = name
+        self.batchnumber = batchnumber
+        self.price = price
+        self.category = category
